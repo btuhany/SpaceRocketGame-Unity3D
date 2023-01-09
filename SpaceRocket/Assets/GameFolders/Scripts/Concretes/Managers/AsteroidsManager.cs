8d7dcc86c522657c4d8a6da6318fadfc7c05ffe3
btuhany <114235184+btuhany@users.noreply.github.com>
2023-01-10 01:27:22 +0300

    Asteroid Mover

--- SpaceRocket/Assets/GameFolders/Scripts/Concretes/Managers/AsteroidsManager.cs
+++ SpaceRocket/Assets/GameFolders/Scripts/Concretes/Managers/AsteroidsManager.cs
@@ -10,9 +10,11 @@ namespace Managers
     public class AsteroidsManager : DangerousObjectsManager
     {
         [SerializeField] Vector3 _direction;
-        [Range(0f, 2f)][SerializeField] float _factor;
+        [SerializeField] float _speed = 1f;
+        [SerializeField] float _factor;
 
         Vector3 _startPosition;
+        
         private void Awake()
         {
             _startPosition= transform.position;
@@ -20,11 +22,14 @@ namespace Managers
 
         private void Update()
         {
+            //sin(2*pi*T*speed) 
+            float sinWave= Mathf.Sin(Mathf.PI*2*Time.time*_speed);
+            _factor = sinWave;
             Vector3 offset= _direction * _factor;
             transform.position = offset + _startPosition;
 
-      
 
+            
         }
     }
 
