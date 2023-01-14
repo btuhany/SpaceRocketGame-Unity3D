cf41b33c26a92787448cfae93c31488fedea97f1
btuhany <114235184+btuhany@users.noreply.github.com>
2023-01-14 18:06:54 +0300

    Better Camera
    CameraScript added to the following camera

---
+++ SpaceRocket/Assets/GameFolders/Scripts/Concretes/Managers/CameraManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] Transform _playerTransform;
+    [SerializeField] private float _lerpTime;
+    private Vector3 _offset;
+
+    private void Start()
+    {
+        _offset = transform.position-_playerTransform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 playerPositionFreezingZ = _playerTransform.position;
+        if(playerPositionFreezingZ.z>=0)
+        {
+            playerPositionFreezingZ.z = 0;
+        }
+        
+
+
+        Vector3 _newPosition=Vector3.Lerp(transform.position, playerPositionFreezingZ + _offset, _lerpTime * Time.deltaTime);
+        transform.position=_newPosition;
+    }
+}
